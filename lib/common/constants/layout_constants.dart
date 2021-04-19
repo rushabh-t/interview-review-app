e2a0b455cd736ccec1ac31af835c62fe14af9ba4
rushabh-t-coditas <you@example.com>
2021-04-19 17:38:28 +0530

    splash screen implemented

--- lib/common/constants/layout_constants.dart
+++ lib/common/constants/layout_constants.dart
@@ -4,4 +4,8 @@ abstract class LayoutConstants {
   static const double designWidth = 375.0;
   static const double designHeight = 812.0;
   static const double dimen_0 = 0.0;
+
+  //splash screen constants
+  static const double splashWidth = 200.0;
+  static const double splashHeight = 70.3;
 }
--- lib/presentation/journey/splash/splash_screen.dart
+++ lib/presentation/journey/splash/splash_screen.dart
@@ -1,8 +1,46 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:interview_review_app/common/constants/app_constants.dart';
+import 'package:interview_review_app/common/constants/layout_constants.dart';
+import 'package:interview_review_app/common/constants/svg_constants.dart';
+import 'package:interview_review_app/common/utils/widget_utils.dart';
+import 'package:interview_review_app/presentation/routes/route_constants.dart';
+import 'package:interview_review_app/presentation/theme/app_color.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Timer _timer;
+
+  @override
+  void initState() {
+    _timer = new Timer(
+      AppConstants.splashDuration,
+      () => Navigator.pushReplacementNamed(context, RouteConstants.home),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.center,
+      color: AppColor.primaryColor,
+      child: getSvgPicture(
+          assetName: SvgConstants.logo,
+          size: Size(
+              LayoutConstants.splashWidth.w, LayoutConstants.splashHeight.h)),
+    );
   }
 }
